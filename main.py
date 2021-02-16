17490a27189ec2102f737a0e562243bf83642532
Foeegarten <77509969+Foeegarten@users.noreply.github.com>
2021-02-16 17:52:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+from discord import *
+import random
+from random import choice
+import os
+rec_films = []
+w_films = []
+intents = discord.Intents.default()
+client = commands.Bot(command_prefix='!', intents=intents)
+@client.command()
+async def add(ctx,*,content:str):
+    rec_films.append(content)
+    print(rec_films)
+    await ctx.send('Добавлено!')
+@client.command()
+async def wat(ctx,*,content:str):
+    if content in rec_films:
+        rec_films.remove(content)
+        w_films.append(content)
+        print(rec_films)
+        print(w_films)
+        await ctx.send("Перемещено")
+    else:
+        await ctx.send("Такого фильма нету в списке")
+@client.command()
+async def spisok_r(ctx):
+    await ctx.send(str(rec_films))
+@client.command()
+async def spisok_w(ctx):
+    await ctx.send(str(w_films))
+@client.command()
+async def recommend(ctx):
+    await ctx.send(random.choice(rec_films))
+token = os.environ.get('BOT_TOKEN')
+client.run(str(token))
